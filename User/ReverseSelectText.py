bfc2c466b39e06f0ee39c63af5ff96679b722d0c
watergear <watergear@gmail.com>
2016-03-22 12:01:41 +0800

    better way to reverse selected text

--- User/ReverseSelectText.py
+++ User/ReverseSelectText.py
@@ -1,14 +1,7 @@
 import sublime, sublime_plugin
 
-from collections import deque
-def reverse(iterable):
-    d = deque()
-    d.extendleft(iterable)
-    return ''.join(d)
-
 class ReverseSelectTextCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         view = self.view
         for s in view.sel():
-            text = reverse(view.substr(s))
-            view.replace(edit, s, text)
+            view.replace(edit, s, view.substr(s)[::-1])
